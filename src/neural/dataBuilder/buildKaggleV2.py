e7578bce88ae0bff7e58cec111936d3d2a016033
theMousaillon <nmorlier22@gmail.com>
2019-01-03 13:50:19 +0100

    update kaggle build

--- src/neural/dataBuilder/buildKaggleV2.py
+++ src/neural/dataBuilder/buildKaggleV2.py
@@ -10,6 +10,14 @@ import time
 
 ## !! premier B à la position 13870 !! ##
 
+def get_lettre(l):
+    def gene():
+        iniLettre = "A"
+        for i in range(26):
+            yield chr(ord(iniLettre) + i)
+    return list(gene())[l]
+
+
 def convertImg(img):
     tmps = ""
     l = []
@@ -38,7 +46,7 @@ def convertImg(img):
 class CalculateChunk():
     def __init__(self, chunk, processNB):
         self.chunk = chunk
-        self.processed = pd.DataFrame(columns=["img", 'Lettre', "imgNF"])
+        self.processed = pd.DataFrame(columns=["img", "imgNF", 'sparse_lettre', "lettre"])
         self.processNB = processNB
         self.progress = 0
 
@@ -48,7 +56,7 @@ class CalculateChunk():
         l = x.split(",")[0]
         img = convertImg(x[len(l)+1:])
         l = int(l)
-        dic = {"img" : img, "Lettre": l, "imgNF": img}
+        dic = {"img" : img, "sparse_lettre": l, "imgNF": normalize(img, axis=1).flatten(), "lettre": get_lettre(l)}
         self.processed = self.processed.append(dic, ignore_index=True)
         return None
 
@@ -78,21 +86,26 @@ def runWorkers(df, nb):
     pool = Pool(processes=nb)
     chunkList = makeChunk(df, nb)
     chunkList = [CalculateChunk(chunkList[i], i+1) for i in range(len(chunkList))]
-    results = [pool.apply(run, args=(x,)) for x in chunkList]
+    results = [pool.apply_async(run, args=(x,)) for x in chunkList]
+    results = [p.get() for p in results]
     return pd.concat(results)
 
-if __name__ == "__main__":
+def main():
     data = pd.read_csv("src/neural/trainingData/bigCsvSample/A_Z Handwritten Data.csv", sep='\n')
     print("début de la construction ...")
     start = time.time()
     print("--> début à : ", start)
-    df = runWorkers(data, 12)
+    df = runWorkers(data, 50)
     end = time.time()
     print("--> fin à : ", end)
     hours, rem = divmod(end-start, 3600)
     minutes, seconds = divmod(rem, 60)
     print("--> dataset construit en : {:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
-    print("--> dataset construit en : ", "")
     print("finished building dataset !!!")
+    return df
+
+if __name__ == "__main__":
+    df = main()
+    df = df.reset_index(drop=True)
     df.to_json("src/neural/trainingData/processedKaggle.json")
 # ipython -m src.neural.dataBuilder.buildKaggleV2
